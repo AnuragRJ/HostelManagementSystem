a1ef334beafc5ec19f2c69a5372dc5ff043f55a0
AnuragRJ <anuragjagtap34@gmail.com>
2026-05-16 22:06:18 +0530

    Add files via upload

---
+++ src/main/java/HostelManagementSystem.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Vishal
+ */
+public class HostelManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
